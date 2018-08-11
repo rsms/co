384b8390224d0aab772c4aeabe862b60f62ae8de
Rasmus Andersson <rasmus@figma.com>
2018-08-11 16:49:52 -0700

    cleans up ir/opt_dce

--- src/ir/opt_dce.ts
+++ src/ir/opt_dce.ts
@@ -36,9 +36,10 @@ export function optdce(fn :Fun) {
       // have to assume all calls have side effects, thus we never elminiate
       // calls.
       //
-      if (v.op === ops.Call || v.op === ops.TailCall || live.delete(v)) {
+      if (v.op.call || live.delete(v)) {
         // register operands as being live (since they are read)
         // dlog(`${v} is live  ${fmtir(v)}`)
+        // TODO: don't eliminate ops with hasSideEffects==true
         if (v.args) for (let operand of v.args) {
           live.add(operand)
         }
@@ -52,44 +53,3 @@ export function optdce(fn :Fun) {
     }
   }
 }
-
-
-// export function optdce(fn :Fun) {
-//   let live = new Set<Value>()  // set of live variables
-//   let b = fn.tailb  // start with last block in function
-//   while (true) {
-//     if (b.control) {
-//       // variable used as branching condition is live at end of block
-//       live.add(b.control)
-//     }
-//     let v = b.vtail
-//     while (v) {
-//       // attempt to remove receiver var from live (except for calls)
-//       //
-//       // TODO: when encountering a call, check if the call has
-//       // side effects (i.e. called function is not pure), and if not attempt
-//       // elimination of the call.
-//       // Currently we don't know if the call has side effects or not, so we
-//       // have to assume all calls have side effects, thus we never elminiate
-//       // calls.
-//       //
-//       if (v.op === ops.Call || v.op === ops.TailCall || live.delete(v)) {
-//         // register operands as being live (since they are read)
-//         // dlog(`${v} is live  ${fmtir(v)}`)
-//         if (v.args) for (let operand of v.args) {
-//           live.add(operand)
-//         }
-//         v = v.prevv
-//       } else {
-//         // dead (the var is not in `live`)
-//         // dlog(`${v} is dead  ${fmtir(v)}`)
-//         v = b.removeValue(v)  // returns previous sibling of v
-//       }
-//       // dumplive(live)
-//     }
-//     if (!b.parent) {
-//       break
-//     }
-//     b = b.parent  // walk parent/previous block
-//   }
-// }
