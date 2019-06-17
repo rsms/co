eea681b208fc4cba524a67085d3fffc2d26eae78
Rasmus Andersson <rasmus@notion.se>
2019-06-16 20:22:24 -0700

    simplify termstyle

--- src/termstyle.ts
+++ src/termstyle.ts
@@ -26,12 +26,6 @@ const TERM = typeof process != 'undefined' && process.env.TERM || ''
 
 export type StyleFun = (s: string) => string
 
-function sfn(open :string, close :string) :StyleFun {
-  open = '\x1b[' + open + 'm'
-  close = '\x1b[' + close + 'm'
-  return (s :string) => open + s + close
-}
-
 // number of colors that the terminal probably supports
 export const termColorSupport :number = (
   TERM && ['xterm','screen','vt100'].some(s => TERM.indexOf(s) != -1) ? (
@@ -43,99 +37,89 @@ export const termColorSupport :number = (
 const passThrough = ((s :string) => s) as StyleFun
 
 export interface Style {
-  'clear'         :string
+  clear         :string
 
-  'bold'          :StyleFun
-  'italic'        :StyleFun
-  'underline'     :StyleFun
-  'inverse'       :StyleFun
+  bold          :StyleFun
+  italic        :StyleFun
+  underline     :StyleFun
+  inverse       :StyleFun
 
-  'white'         :StyleFun
-  'grey'          :StyleFun
-  'black'         :StyleFun
-
-  'blue'          :StyleFun
-  'cyan'          :StyleFun
-  'green'         :StyleFun
-  'magenta'       :StyleFun
-  'purple'        :StyleFun
-  'pink'          :StyleFun
-  'red'           :StyleFun
-  'yellow'        :StyleFun
-  'lightyellow'   :StyleFun
-  'orange'        :StyleFun
+  white         :StyleFun
+  grey          :StyleFun
+  black         :StyleFun
+  blue          :StyleFun
+  cyan          :StyleFun
+  green         :StyleFun
+  magenta       :StyleFun
+  purple        :StyleFun
+  pink          :StyleFun
+  red           :StyleFun
+  yellow        :StyleFun
+  lightyellow   :StyleFun
+  orange        :StyleFun
 }
 
 export const noStyle :Style = {
   // no colors
-  'clear'         : "",
+  clear         : "",
 
-  'bold'          : passThrough,
-  'italic'        : passThrough,
-  'underline'     : passThrough,
-  'inverse'       : passThrough,
+  bold          : passThrough,
+  italic        : passThrough,
+  underline     : passThrough,
+  inverse       : passThrough,
 
-  'white'         : passThrough,
-  'grey'          : passThrough,
-  'black'         : passThrough,
-  'blue'          : passThrough,
-  'cyan'          : passThrough,
-  'green'         : passThrough,
-  'magenta'       : passThrough,
-  'purple'        : passThrough,
-  'pink'          : passThrough,
-  'red'           : passThrough,
-  'yellow'        : passThrough,
-  'lightyellow'   : passThrough,
-  'orange'        : passThrough,
+  white         : passThrough,
+  grey          : passThrough,
+  black         : passThrough,
+  blue          : passThrough,
+  cyan          : passThrough,
+  green         : passThrough,
+  magenta       : passThrough,
+  purple        : passThrough,
+  pink          : passThrough,
+  red           : passThrough,
+  yellow        : passThrough,
+  lightyellow   : passThrough,
+  orange        : passThrough,
 }
+
+const sfn = (
+  termColorSupport < 256 ?
+  (open :string, _ :string, close :string) :StyleFun => {
+    open = '\x1b[' + open + 'm'
+    close = '\x1b[' + close + 'm'
+    return (s :string) => open + s + close
+  } :
+  (_ :string, open :string, close :string) :StyleFun => {
+    open = '\x1b[' + open + 'm'
+    close = '\x1b[' + close + 'm'
+    return (s :string) => open + s + close
+  }
+)
 
 export const style :Style = (
   termColorSupport == 0 ? noStyle :
-  termColorSupport < 256 ? {
-    // 16 colors
-    'clear'         : "\e[0m",
+  {
+    clear         : "\e[0m",
 
-    'bold'          : sfn('1', '22'),
-    'italic'        : sfn('3', '23'),
-    'underline'     : sfn('4', '24'),
-    'inverse'       : sfn('7', '27'),
+    bold          : sfn('1', '1', '22'),
+    italic        : sfn('3', '3', '23'),
+    underline     : sfn('4', '4', '24'),
+    inverse       : sfn('7', '7', '27'),
 
-    'white'         : sfn('37', '39'),
-    'grey'          : sfn('90', '39'),
-    'black'         : sfn('30', '39'),
-    'blue'          : sfn('34', '39'),
-    'cyan'          : sfn('36', '39'),
-    'green'         : sfn('32', '39'),
-    'magenta'       : sfn('35', '39'),
-    'purple'        : sfn('35', '39'),
-    'pink'          : sfn('35', '39'),
-    'red'           : sfn('31', '39'),
-    'yellow'        : sfn('33', '39'),
-    'lightyellow'   : sfn('93', '39'),
-    'orange'        : sfn('33', '39'),
-  } : {
-    // 256 colors
-    'clear'         : "\e[0m",
-
-    'bold'          : sfn('1', '22'),
-    'italic'        : sfn('3', '23'),
-    'underline'     : sfn('4', '24'),
-    'inverse'       : sfn('7', '27'),
-
-    'white'         : sfn('38;2;255;255;255', '39'),
-    'grey'          : sfn('38;5;244', '39'),
-    'black'         : sfn('38;5;16',  '39'),
-    'blue'          : sfn('38;5;75',  '39'), // '38;2;120;160;255'
-    'cyan'          : sfn('38;5;87',  '39'),
-    'green'         : sfn('38;5;84',  '39'),
-    'magenta'       : sfn('38;5;213', '39'),
-    'purple'        : sfn('38;5;141', '39'),
-    'pink'          : sfn('38;5;211', '39'),
-    'red'           : sfn('38;2;255;110;80', '39'),
-    'yellow'        : sfn('38;5;227', '39'),
-    'lightyellow'   : sfn('38;5;229', '39'),
-    'orange'        : sfn('38;5;215', '39'),
+    white         : sfn('37', '38;2;255;255;255', '39'),
+    grey          : sfn('90', '38;5;244', '39'),
+    black         : sfn('30', '38;5;16', '39'),
+    blue          : sfn('34', '38;5;75', '39'),
+    cyan          : sfn('36', '38;5;87', '39'),
+    green         : sfn('32', '38;5;84', '39'),
+    magenta       : sfn('35', '38;5;213', '39'),
+    purple        : sfn('35', '38;5;141', '39'),
+    pink          : sfn('35', '38;5;211', '39'),
+    red           : sfn('31', '38;2;255;110;80', '39'),
+    yellow        : sfn('33', '38;5;227', '39'),
+    lightyellow   : sfn('93', '38;5;229', '39'),
+    orange        : sfn('33', '38;5;215', '39'),
   }
 )
 
